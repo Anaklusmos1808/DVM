0467fb6db2b8866dfe8dc285f771eee0bbe6f6f1
Anaklusmos1808 <116489378+Anaklusmos1808@users.noreply.github.com>
2022-12-13 00:02:42 +0530

    Update _script.py

--- _script.py
+++ _script.py
@@ -1,3 +1,13 @@
-def populate_books(self):
-        import _script
-        _script.populate(self)
+import openpyxl as opx
+
+def populate(shelf_instance):
+    import Task_1
+    file = opx.load_workbook('books.xlsx')
+    sheet = file['Sheet1']
+
+    for row in sheet.values:
+        if row[0] == 'name' and row[1] == 'isbn' and row[2] == 'author':
+            continue
+
+        else:
+            shelf_instance.shelf_arr.append(Task_1.Book(row[0], row[2], row[1]))
